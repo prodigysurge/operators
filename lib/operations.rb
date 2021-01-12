953eda263aaf2d6192b1170d0e759ae9b4dcedcb
prodigysurge <boatinglife6@yahoo.com>
2021-01-12 14:36:16 -0500

    commit tree 99b47d411a9ba813590eea6ee177b0419c0188e4

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
-
+    if speed > 60
+        return true
+    elsif speed < 40
+        return true
+    else false
+     end
 end
-
-
 
 def not_safe?(speed)
-	
+    if  speed > 60 
+        return true
+    else speed < 40 ? true : false
+    end
 end
 	
+
 
 
